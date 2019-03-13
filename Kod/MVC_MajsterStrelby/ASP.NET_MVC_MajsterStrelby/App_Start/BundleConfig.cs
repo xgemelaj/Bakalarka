4266e26c21d3554913fde2c9dc7690d24ea34d18
unknown <gemela.juraj@gmail.com>
2019-03-13 01:03:13 +0100

    Update view 2.0 - Bootstrap 4.3.1, fixed all issue with new bootstrap, and game mode with characters implement

--- Kod/MVC_MajsterStrelby/ASP.NET_MVC_MajsterStrelby/App_Start/BundleConfig.cs
+++ Kod/MVC_MajsterStrelby/ASP.NET_MVC_MajsterStrelby/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ASP.NET_MVC_MajsterStrelby
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
