c567329593c888fbf78e78cc595a5bb0179f6b13
unknown <gemela.juraj@gmail.com>
2019-03-27 23:35:36 +0100

    Publish + presentation_begin

--- Kod/MVC_MajsterStrelby/ASP.NET_MVC_MajsterStrelby/Models/ResultViewModel.cs
+++ Kod/MVC_MajsterStrelby/ASP.NET_MVC_MajsterStrelby/Models/ResultViewModel.cs
@@ -41,12 +41,11 @@ namespace ASP.NET_MVC_MajsterStrelby.Models
             
             //Make order of words to challenging taskword
 
-            querry = "SELECT part.druhe_slovo, part.points * 1.0/part.amount * 1.0 as coefficient " +
-                            "FROM( " +
-                                "SELECT druhe_slovo, SUM(vzdialenost) as points, COUNT(druhe_slovo) as amount " +
-                                "FROM zbieranie_ohodnoteni WHERE prve_slovo LIKE @TaskWord GROUP BY druhe_slovo " +
-                            ") as part " +
-                            "WHERE part.points > 0 " +
+            querry = "SELECT druhe_slovo, COUNT(druhe_slovo) as amount, SUM(vzdialenost) * 1.0 / COUNT(druhe_slovo) * 1.0 as coefficient " +
+                            "FROM zbieranie_ohodnoteni " +
+                            "WHERE prve_slovo LIKE @TaskWord " +
+                            "GROUP BY druhe_slovo " +
+                            "HAVING SUM(vzdialenost) > 0 " +
                             "ORDER BY coefficient DESC, amount ASC";
             dT = new DataTable();
 
---
+++ Kod/MVC_MajsterStrelby/ASP.NET_MVC_MajsterStrelby/bin/Release/Publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC_MajsterStrelby.MvcApplication" Language="C#" %>
