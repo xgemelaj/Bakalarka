21abfa371b2b1de6463380bbd450b4ae71b53fd2
unknown <gemela.juraj@gmail.com>
2019-05-02 15:24:12 +0200

    Before showing

--- Kod/MVC_MajsterStrelby/getting_data.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT part.druhe_slovo, part.points, part.amount, part.points * 1.0/part.amount * 1.0 as coefficient 
-FROM (
-	SELECT druhe_slovo, SUM(vzdialenost) as points, COUNT(druhe_slovo) as amount 
-	FROM zbieranie_ohodnoteni WHERE prve_slovo LIKE 'diskutabilný' GROUP BY druhe_slovo
-	) as part 
-ORDER BY coefficient DESC, amount ASC
